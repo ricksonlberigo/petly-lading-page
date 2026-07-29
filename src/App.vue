34cddd0330fa5d5640c79a6fc2dcebc6b8fb7c77
ricksonl.berigo <ricksonl.berigo@outlook.com>
2026-07-29 16:41:41 -0300

    feat: add Vercel Analytics for improved tracking and insights in App component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import { Analytics } from '@vercel/analytics/vue'
 import BackToTopButton from '@/components/ui/BackToTopButton.vue'
 </script>
 
 <template>
   <RouterView />
   <BackToTopButton />
+  <Analytics />
 </template>
